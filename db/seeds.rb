ca683adf20d8d3730255ca8ec70aa9212edca8c3
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-11-09 19:16:51 -0500

    added the raw data for 100 addresses in each district

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 #SEEDING THE LOCATIONS:
+REGIONS = ["NW", "WC", "EC", "SW", "SE"]
+REGIONS.each do |region|
+  number = 1
+  until number == 101
+    Location.create(region: region, number: number)
+    number += 1
+  end
+end
